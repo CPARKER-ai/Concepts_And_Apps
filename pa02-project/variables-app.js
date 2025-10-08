c5338a78b642320292d0bd67822d4a9b9df17f6c
CPARKER-ai <116286076+CPARKER-ai@users.noreply.github.com>
2025-10-07 20:33:11 -0400

    Update and rename strings-app.js to variables-app.js
    Variable names are included in this commit, for x, y, z and b, each with their respective values and/or operations.

--- pa02-project/variables-app.js
+++ pa02-project/variables-app.js
@@ -3,8 +3,16 @@ console.log("Welcome to my JavaScript Advanced Website");
 console.log("Repo URL: https://github.com/CPARKER-ai/Concepts_And_Apps/tree/PA02");
 
 // Strings
-let Identification = "Jonathan Crambell"; 
+let identification = "Jonathan Crambell"; 
 let IDNumber = "300403789"; 
 let todaysDate = "10-7-2025"; 
+
+// Variables
+let x = 10; 
+let y = 20; 
+let z = x*y; 
+const b = 21; 
+
+
 
 
