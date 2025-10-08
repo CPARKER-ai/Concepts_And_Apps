724d243b791b8230f347de7e108ccf27b91b3caa
CPARKER-ai <116286076+CPARKER-ai@users.noreply.github.com>
2025-10-07 20:29:59 -0400

    Update and rename app.js to strings-app.js
    Added in my string names involved with apps.js.

--- pa02-project/strings-app.js
+++ pa02-project/strings-app.js
@@ -1,3 +1,10 @@
 console.log("Welcome to my JavaScript Advanced Website"); 
 
 console.log("Repo URL: https://github.com/CPARKER-ai/Concepts_And_Apps/tree/PA02");
+
+// Strings
+let Identification = "Jonathan Crambell"; 
+let IDNumber = "300403789"; 
+let todaysDate = "10-7-2025"; 
+
+
